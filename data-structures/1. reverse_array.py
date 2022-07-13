6d063459fc3f3e137b3ae6f7d742b0927487047c
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-13 21:04:50 +0300

    reverse

--- data-structures/1. reverse_array.py
+++ data-structures/1. reverse_array.py
@@ -1,20 +1,18 @@
-def reverse_array(a):
-    # method 1 - using the reversed() built-in function
-    # return [ele for ele in reversed(a)]
-
-    # method 2 - using slicing technique
-    return a[::-1]
-
-
+# initialize empty array
+arr = []
 # specify array length
-arr_count = int(input().strip())
+arr_count = int(input("Enter number of array elements: ").strip())
 
 # declare array variable
-arr = list(map(int, input().rstrip().split()))
+for values in enumerate(range(0, arr_count)):
+    arr_ele = int((input("Enter element {0}: ".format(values[0]))))
+    arr.append(arr_ele)
+# arr = list(map(int, input("Enter a number string e.g 37648937: ").rstrip().split()))
 print("Original array: {}".format(arr))
 
-# reversed result
-res = reverse_array(arr)
+# reverse method 1. slicing
+# print("Reversed array: {0}".format(arr[::-1]))
 
-# display reversed result
-print("Reversed array: {}".format(res))
+# method 1 - using the reversed() built-in function
+print("Reversed array: {0}".format(list(reversed(arr))))
+#[ele for ele in reversed(a)]
