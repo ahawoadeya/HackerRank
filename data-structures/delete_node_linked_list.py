f8a5a1ba89a8d897634e512ebddabe995d42567d
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-15 23:22:15 +0300

    delete-node

--- data-structures/delete_node_linked_list.py
+++ data-structures/delete_node_linked_list.py
@@ -27,46 +27,54 @@ class Node:
         self.next = None
 
 
-def get_node(data):
-    """function to create and return a Node"""
+class LinkedList:
+    """implementation"""
 
-    # allocating space
-    new_node = Node(data)
-    return new_node
+    # initialize head
+    def __init__(self):
+        self.head = None
 
-def delete_node(head_node, position):
-    """ function to delete a node at a specific position"""
+    def push(self, new_data):
+        """insert new node at the beginning"""
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
-    head = head_node
+    def delete_node(self, head_node, position):
+        """ function to delete a node at a specific position"""
 
-    if position < 0:
-        print("Invalid position")
+        head = head_node
 
-    if position == 0:
-        head = head.next
+        if position < 0:
+            print("Invalid position")
 
-    return head
+        if position == 0:
+            head = head.next
+
+        return head
 
 
-def print_list(head_node):
-    """print list contents"""
-    while head_node is not None:
-        print(' ' + str(head_node.data), end='')
-        head_node = head_node.next
-    print()
+    def print_list(self, head_node):
+        """print list contents"""
+        while head_node is not None:
+            print(' ' + str(head_node.data), end='')
+            head_node = head_node.next
+        print()
 
 # driver code
 if __name__ == '__main__':
-    new_head = get_node(23)
-    new_head.next = get_node(85)
-    new_head.next.next = get_node(76)
-    new_head.next.next.next = get_node(38)
+    llist = LinkedList()
+
+    llist_elements = int(input("Enter number of elements in the linked list: ").strip())
+    for values in enumerate(range(0, llist_elements)):
+        llist_ele = int(input("Enter llist integer {0}: ".format(values[0])).strip())
+        llist.push(llist_ele)
 
     print("Linked list before deleting:", end='')
-    print_list(new_head)
+    llist.print_list(llist.head)
 
     position_to_delete = int(input("Enter position to delete: ").strip())
-    new_head = delete_node(new_head, position_to_delete)
+    new_head = llist.delete_node(llist.head, position_to_delete)
 
     print("Linked list after deleting at position {0}: ".format(position_to_delete))
-    print_list(new_head)
+    llist.print_list(new_head)
